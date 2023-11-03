4b3aae74ae68064537d7b6007447d581e405a0b7
Sinan Cay <sinan_cy@hotmail.com>
2023-11-03 17:51:59 +0300

    4- 5 has been uploaded

--- 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,25 +10,54 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        private static Semaphore s = new Semaphore(1, 10);  // Available=1; Capacity=10
+
         static void Main(string[] args)
         {
-            Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
-            Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
-            Console.WriteLine("Implement all of the following options:");
-            Console.WriteLine();
-            Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
-            Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
+            // Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
+            // Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
+            // Console.WriteLine("Implement all of the following options:");
+            // Console.WriteLine();
+            // Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
+            // Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
 
             Console.WriteLine();
-
+            
             // feel free to add your code
+            
+            Thread t = new Thread(() => Worker(10));
+            t.Name = "1";
+            t.Start();
 
             Console.ReadLine();
+        }
+
+        private static void Worker(int count)
+        {
+                if (count == 0) {
+                    Console.WriteLine("Last Thread its end");
+                }else {
+                        s.WaitOne();
+                            Console.WriteLine("Thread " + count + " Processing");
+                            count -= 1;
+                            Thread t = new Thread(() => Worker(count));
+                            t.Start();
+                        s.Release();
+                    try 
+                    {
+                        t.Join();
+                    } 
+                    catch (Exception e) 
+                    {
+                       Console.WriteLine("Well... That didn't go as planned!");
+                    }
+            }
         }
     }
 }
--- 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,21 +5,55 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Threading; 
+using System.Collections.Generic; 
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        static List<int> collections = new List<int>();
+        const int limit = 10; 
         static void Main(string[] args)
         {
-            Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
-            Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
-            Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
-            Console.WriteLine();
+            // Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
+            // Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
+            // Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
+            // Console.WriteLine();
 
             // feel free to add your code
 
+                Thread thread = new Thread(AddMethod);
+                thread.Start();
+
+            
+           
+
             Console.ReadLine();
         }
+        static void AddMethod()  
+        {  
+             lock (collections)
+            {
+                    for (int i = 0; i < limit; i++)
+                    {
+                        collections.Add(i);
+                        Thread thread = new Thread(PrintMethod); // same second call print worker thread
+                        thread.Start();
+                        Thread.Sleep(1000);
+                    }
+                }
+        }  
+        static void PrintMethod()  
+        {  
+             lock (collections)
+            {
+                foreach (var item in collections)
+                {
+                    Console.WriteLine(item);
+                }
+                Thread.Sleep(1000);
+            }
+        }  
     }
 }
